b3b30fe8220dd7ac58b2f48233e34bc3114a6a90
Damien McCormick <DMcCormick20172017@outlook.com>
2024-06-01 23:46:38 -0700

    Added more stuff to cmn script

--- Archives/Char/Cmn/CmnScript.lua
+++ Archives/Char/Cmn/CmnScript.lua
@@ -1,1 +1,19 @@
 import("System.Numerics")
+import("BooBoo", "BooBoo.Battle")
+import("BooBoo", "BooBoo.Util")
+
+local oneSixteth <const> = 1/60
+
+function Sign(x)
+	if x > 0 then
+		return 1
+	elseif x < 0 then
+		return -1
+	else
+		return 0
+		end
+	end
+
+function HitVector(x, y)
+	return Vector2(-x * oneSixteth, y * oneSixteth)
+	end
